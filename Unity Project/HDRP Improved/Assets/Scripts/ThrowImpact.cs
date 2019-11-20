d3a074b0263b030aaa09ca604087e8c5d24212d1
Ozker24 <oscarcerro4@gmail.com>
2019-11-20 23:49:31 +0100

    intentsando hacer la parabola

--- Unity Project/HDRP Improved/Assets/Scripts/ThrowImpact.cs
+++ Unity Project/HDRP Improved/Assets/Scripts/ThrowImpact.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class ThrowImpact : MonoBehaviour
 {
-    public float distance;
-    public float radius;
     public PlayerController player;
-    public Vector3 whereToPoint;
+    public Items items;
+    public float distance;
+    public float high;
+    public float force;
+    [SerializeField] float time;
+    public Vector3 parabolVect;
 
     public void Initialize()
     {
-        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        items = player.items;
     }
 
     public void MyUpdate()
     {
-        whereToPoint = player.modelTrans.transform.forward;
+        high = items.throwTrans.position.y;
+        parabolVect = items.cam.transform.forward;
+        //force = items.throwForce;
+
+        CalculateTime();
+        CalculatePos();
+
+        transform.position = (transform.position + Vector3.forward * distance);
     }
 
-    private void OnDrawGizmos()
+    public void CalculateTime()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(new Vector3(player.modelTrans.position.x, player.modelTrans.position.y, player.modelTrans.position.z + distance), radius);
+        float FSin = -force * parabolVect.y;
+        float Fsin2 = ((force * parabolVect.y) * (force * parabolVect.y));
+        float FourGrab = -4 * ( -4.9f ) * high;
+        float Grab = 2 * (- 4.9f);
+        time = (FSin - Mathf.Sqrt(Fsin2 - FourGrab) / Grab);
     }
 
-    public Vector3 ReturnDir()
+    public void CalculatePos()
     {
-        Vector3 dir = player.camTrans.TransformDirection(whereToPoint);
-
-        dir.Set(dir.x, 0, dir.z);
-
-        return dir.normalized * whereToPoint.magnitude;
-
+        distance = force * parabolVect.z * time;
     }
 }
