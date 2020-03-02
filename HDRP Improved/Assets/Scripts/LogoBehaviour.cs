c3eacf989564c20708a1b9d54b2c677c759f6657
Another account <Another account>
2020-03-02 10:10:01 +0100

    Implementing Logo
    Thats it

--- HDRP Improved/Assets/Scripts/LogoBehaviour.cs
+++ HDRP Improved/Assets/Scripts/LogoBehaviour.cs
@@ -3,98 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class LogoBehaviour : MonoBehaviour
 {
-    public enum State { FadeIn, FadeOut, Waiting};
-    public State states;
-
-    [Header("FadeInSources")]
-    public Image toFadeIn;
-    public Color fadeInColor;
-
-    [Header("FadeOutSources")]
-    public Image toFadeOut;
-    public Color fadeOutColor;
-
-    [Header("Time Variables")]
-    public float timeCounter;
-    public float timeOfFadeIn;
-    public float timeOfFadeOut;
-    public float timeOfWait;
+    public VideoPlayer vidPlayer;
+    public bool endWaitSec;
 
     public void Start()
     {
+        vidPlayer = GetComponent<VideoPlayer>();
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-        fadeInColor.a = 0;
-        toFadeIn.color = fadeInColor;
-
-        fadeOutColor.a = 0;
-        toFadeOut.color = fadeOutColor;
+        StartCoroutine(SetWaitSec());
     }
 
     public void Update()
     {
         Skiplogo();
-
-        switch (states)
-        {
-            case State.FadeIn:
-            FadeIn();
-            break;
-            case State.Waiting:
-            Waiting();
-            break;
-            case State.FadeOut:
-            FadeOut();
-            break;
-        }
-    }
-
-    public void FadeIn()
-    {
-        if (fadeInColor.a < 1)
-        {
-            fadeInColor.a += Time.deltaTime / timeOfFadeIn;
-        }
-        else
-        {
-            fadeInColor.a = 1;
-            states = State.Waiting;
-        }
-
-        toFadeIn.color = fadeInColor;
-    }
-
-    public void FadeOut()
-    {
-        if (fadeOutColor.a < 1)
-        {
-            fadeOutColor.a += Time.deltaTime / timeOfFadeOut;
-        }
-        else
-        {
-            fadeOutColor.a = 1;
-            SceneManager.LoadScene(1);
-            Cursor.lockState = CursorLockMode.None;
-        }
-
-        toFadeOut.color = fadeOutColor;
-    }
-
-    public void Waiting()
-    {
-        if (timeCounter >= timeOfWait)
-        {
-            timeCounter = 0;
-            states = State.FadeOut;
-        }
-        else
-        {
-            timeCounter += Time.deltaTime;
-        }
+        GoToMenu();
     }
 
     void Skiplogo()
@@ -104,5 +33,27 @@ public class LogoBehaviour : MonoBehaviour
             SceneManager.LoadScene(1);
             Cursor.lockState = CursorLockMode.None;
         }
+    }
+
+    void GoToMenu()
+    {
+        if(endWaitSec)
+        {
+            if (!vidPlayer.isPlaying)
+            {
+                SceneManager.LoadScene(1);
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+
+    IEnumerator SetWaitSec()
+    {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        endWaitSec = true;
     }
 }
